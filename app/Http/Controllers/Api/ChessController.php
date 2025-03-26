3f8d10cd17faa8b28694430d37ea37a17cf4a0f9
Steve <stefan.masan@uniit.sk>
2025-03-26 21:27:54 +0100

    next

--- app/Http/Controllers/Api/ChessController.php
+++ app/Http/Controllers/Api/ChessController.php
@@ -27,6 +27,19 @@ class ChessController extends Controller
         $prompt = 'Given the FEN string 
         ' . $request->fen . ' and the color ' . $request->color . ', generate a move. 
         THe move should be in format e4-e5 and return the move only without anything else.';
+        
+        $data = [
+            "contents" => [
+                [
+                    "parts" => [
+                        [
+                            "text" => $prompt,
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
         try {
             // STEP 4: Send a POST request to the Gemini API Using Illuminate\Support\Facades\Http https://laravel.com/docs/11.x/http-client#headers
 
